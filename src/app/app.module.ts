e51b9c9714ea39973c97d11f0d50c5f3960612ab
marcoa5 <marco.arato@gmail.com>
2021-02-08 17:23:06 +0100

    b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './comp/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { MainComponent } from './comp/main/main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/comp/main/main.component.ts
+++ src/app/comp/main/main.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth'
+import { of } from 'rxjs';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeBGl6Y21nRXiQNYpDsaLt1P5EtxkWoJQ",
+  authDomain: "epirocttm.firebaseapp.com",
+  projectId: "epirocttm",
+  storageBucket: "epirocttm.appspot.com",
+  messagingSenderId: "619644570924",
+  appId: "1:619644570924:web:9e12534007aac2c98bb089"
+};
 
 @Component({
   selector: 'app-main',
@@ -6,10 +19,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
-
+userName: any;
+  constructor() { 
+    firebase.initializeApp(firebaseConfig) 
+  }
+  
   ngOnInit(): void {
+    firebase.auth().onAuthStateChanged(user=>{
+      if(user){
+        this.userName = user?.displayName
+      } else {
+        this.userName="na"
+      }
+      
+    })
+  }
+
+  login(){
+    let provider = new firebase.auth.OAuthProvider('microsoft.com');
+    provider.setCustomParameters({
+      prompt: 'consent',
+      tenant: '896ecbea-bd27-4a3c-a131-34aa0b46a086'
+    });
+    firebase.auth().signInWithRedirect(provider)
+
+   
   }
 
 }
